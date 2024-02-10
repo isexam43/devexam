dd3ea59e636def00c22057f0f75508b3b58a6582
isexam43 <159609204+isexam43@users.noreply.github.com>
2024-02-10 12:29:21 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,8 @@
+public class example{
+    public static void main(String args[]){
+        int a=5;
+        int b=5;
+        int c=a+b;
+        System.out.println("the sum of a and b is "+ c);
+    }
+}
